287944c065815c8b1d187b483bcd77819680a7e5
大辉Face <okaywit@163.com>
2015-07-23 18:52:45 +0800

    百度抓取

--- src/com/bbcow/platform/BaiduPing.java
+++ src/com/bbcow/platform/BaiduPing.java
@@ -1,6 +1,11 @@
 package com.bbcow.platform;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.HttpClient;
@@ -9,6 +14,41 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 public class BaiduPing {
+        public static void site() {
+                //TODO
+                URL url;
+                StringBuffer sbf = new StringBuffer();
+                try {
+                        url = new URL("http://data.zz.baidu.com/urls?site=www.grownbook.com&token=hqjEuHTIHexJiPFt");
+                        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                        connection.setRequestMethod("POST");
+                        connection.setRequestProperty("Content-Type", "text/plain");
+                        connection.setRequestProperty("User-Agent", "curl/7.12.1");
+                        connection.setDoInput(true);
+                        connection.setDoOutput(true);
+                        connection.setUseCaches(false);
+                        connection.connect();
+
+                        OutputStream outStrm = connection.getOutputStream();
+                        ObjectOutputStream objOutputStrm = new ObjectOutputStream(outStrm);
+                        objOutputStrm.writeChars(new String("http://www.grownbook.com/share.html"));
+                        objOutputStrm.writeChars(new String("http://www.grownbook.com/index.html"));
+                        objOutputStrm.flush();
+
+                        objOutputStrm.close();
+
+                        InputStream inStrm = connection.getInputStream();
+
+                        System.out.println(sbf.toString());
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+        }
+
+        public static void main(String[] args) {
+                BaiduPing.site();
+        }
+
         public static void ping() {
                 HttpClient client = new DefaultHttpClient();
                 try {
@@ -23,13 +63,13 @@ public class BaiduPing {
                                 .append("<value><string>八牛号外</string></value>")
                                 .append("</param>")
                                 .append("<param>")
-                                .append("<value><string>http://grownbook.com/</string></value>")
+                                .append("<value><string>http://www.grownbook.com/</string></value>")
                                 .append("</param>")
                                 .append("<param>")
-                                .append("<value><string>http://grownbook.com/</string></value>")
+                                .append("<value><string>http://www.grownbook.com/</string></value>")
                                 .append("</param>")
                                 .append("<param>")
-                                .append("<value><string>http://grownbook.com/rss</string></value>")
+                                .append("<value><string>http://www.grownbook.com/rss</string></value>")
                                 .append("</param>")
                                 .append("</params>")
                                 .append("</methodCall>");
